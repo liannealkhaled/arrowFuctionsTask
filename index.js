c5d996eae748f81fae0ddcc248ecdc44135b4685
lianne <liannealq@hotmail.com>
2023-09-20 12:42:45 +0300

    20/9

--- index.js
+++ index.js
@@ -5,24 +5,47 @@
 	}
 ********************************/
 
-const greet = (x) => `hello ,${x}`
+
+const greet = (x) => `hello, ${x}`
+
 
 console.log(greet(`lianne`))
 
 
 
+
 // Q2) Write a simple arrow function that takes two parameters and returns their sum.
-const sum = (x,y) => x+y 
-console.log(sum(2,3))
+
+let sum = (x,y) => x+y 
+
+console.log(sum(5,10))
+
+
+// const sum = (x,y) => x+y 
+// console.log(sum(2,3))
 
 // Q3) Write a simple arrow function that squares a number.
+let sqaure = (x) => x*x 
+
+console.log(sqaure(2))
+
+
 const squared = (x) => x*x
 console.log(squared(4))
 
 // Q4) [🌶️🌶️] Create an arrow function that takes an array of numbers and returns a new array with each number squared.
-const arr = [ 1,2,3,4]
-let newarray = []
+let array = [ 1,2,3,4,5]
+let newarray = [ ]
 
-newarray =arr.map (function(x){return x*x})
+newarray = array.map( (x)=> x*x )
+
 
 console.log(newarray)
+
+
+// const arr = [ 1,2,3,4]
+// let newarray = []
+
+// newarray =arr.map (function(x){return x*x})
+
+// console.log(newarray)
